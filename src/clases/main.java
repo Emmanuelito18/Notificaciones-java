52dd148f429a3d0adfc305fce707ba52a880805a
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-01-10 23:08:09 -0600

    Se añadió código parcial que crea notificacion java en main.java

--- src/clases/main.java
+++ src/clases/main.java
@@ -6,13 +6,74 @@
 package clases;
 
 import formularios.principalNotificaciones;
+//<editor-fold defaultstate="collapsed" desc="Librerias notificacion java">
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+//</editor-fold>
 
 /**
  *
  * @author Emmanuel
  */
 public class main {
+    //<editor-fold defaultstate="collapsed" desc="Notificacion java">
+    PopupMenu popup=new PopupMenu();
+    private Image imagen=new ImageIcon(getClass().getResource("/imagenes/notificacion.png")).getImage();
+    private final TrayIcon trayIcon=new TrayIcon(imagen,"Notificación java",popup);
+    //obtiene instancia SsytemTray
+    final SystemTray systemTray=new SystemTray.getSystemTray();
+    //Para el timer
+    private Timer contador;
+    public static boolean bandera;
+    
+    void Notificacion(){
+        //comprueba si systemTray es soportado en el sistema
+        if(SystemTray.isSupported()){
+            //acciones del raton sobre el icono del la barra de tareas
+            MouseListener mouseListener=new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    //Si se presiono el boton izquierdo y la aplicacion esta minimizada
+                    if (e.getButton() == MouseEvent.BUTTON1)// && miframe.getExtendedState()==JFrame.ICONIFIED )
+                    {
+                        MensajeTrayIcon("Por favor verifique la informaciÃ³n", TrayIcon.MessageType.WARNING);
+                    }
+                }
 
+                @Override
+                public void mouseEntered(MouseEvent evt) {
+                }
+
+                @Override
+                public void mouseExited(MouseEvent evt) {
+                }
+
+                @Override
+                public void mousePressed(MouseEvent evt) {
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent evt) {
+                }
+            };
+            //acciones del menu popup
+        }
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Función principal del programa">
     /**
      * @param args the command line arguments
      */
@@ -22,5 +83,5 @@ public class main {
         p.setVisible(true);
         p.setLocationRelativeTo(null);
     }
-    
+    //</editor-fold>
 }
